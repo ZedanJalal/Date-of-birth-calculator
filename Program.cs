3f401cbddf9dd4b67f668ec6114e94ce69dcab19
ZedanJalal <77440443+ZedanJalal@users.noreply.github.com>
2021-01-14 14:12:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Date_of_birth_calculator_
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int year = 2021;
+            Console.WriteLine("Please enter your age");
+            int age =int.Parse(Console.In.ReadLine());
+            int YearOfBirth = year - age;
+            Console.WriteLine("YearOfBirth is " + YearOfBirth);
+        }
+    }
+}
